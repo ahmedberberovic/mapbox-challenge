cbdaedb6cb5818028dd75670ea1af4f37a288811
ahmedberberovic <ahmed_berberovic@hotmail.com>
2020-01-25 16:25:52 +0100

    saturday commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,166 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import ReactMapboxGl, { GeoJSONLayer } from 'react-mapbox-gl';
+import DrawControl from "react-mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import Slider from '@material-ui/core/Slider';
 
-function App() {
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiYWhtZWRiaDkiLCJhIjoiY2s1bzZkbmQ1MDNzMTNzcWw0eXJpc3duNiJ9.RcZZI2_JbPoeFp-14N8y8w'
+
+});
+
+let valueLength=0.037;
+
+export default class App extends Component {
+
+  state = {
+    value: 0.037,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+    valueLength=value;
+  };
+ 
+
+  onDrawCreate = ({ features }) => {
+    console.log(features);
+  };
+
+  onDrawUpdate = ({ features }) => {
+    console.log({ features });
+  };
+  _onClickMap(map, evt) {
+
+    if (map.getLayer('route')) map.removeLayer('route');
+    if (map.getSource('routesource')) map.removeSource('routesource');
+
+    console.log(evt.lngLat);
+    document.getElementById('info').innerHTML =
+      JSON.stringify(evt.lngLat.wrap());
+    let lng=evt.lngLat.lng;
+    let lat=evt.lngLat.lat;
+    map.addSource('routesource', {
+          type: 'geojson',
+          data: {
+            "type": "FeatureCollection",
+            "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "stroke": "#555555",
+              "stroke-width": 3,
+              "stroke-opacity": 1
+            },
+            "geometry": {
+              "type": "LineString",
+              "coordinates": [
+                [
+                  lng,
+                  lat
+                ],
+                [
+                  lng+valueLength,
+                  lat
+                ]
+              ]
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "stroke": "#555555",
+              "stroke-width": 3,
+              "stroke-opacity": 1
+            },
+            "geometry": {
+              "type": "LineString",
+              "coordinates": [
+                [
+                  lng,
+                  lat+0.0013
+                ],
+                [
+                  lng+valueLength,
+                  lat+0.0013
+                ]
+              ]
+            }
+          }
+        ]
+      }
+
+        });
+    map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'routesource',
+            
+            'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+            },
+            'paint': {
+            'line-color': '#0044FF',
+            'line-width': 5
+            }
+        });
+  }
+
+render() {
+  
+  const { value } = this.state;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+    <pre id="info"></pre>
+      <Map
+        style="mapbox://styles/mapbox/satellite-v9"
+        center={[28.672388600125828,41.062677228593344]}
+        containerStyle={{
+          height: '80vh',
+          width: '100vw'
+        }}
+        zoom={[10]}
+        //center={[-122.48369693756104, 37.83381888486939]}
+        //center={[-73.97777080535889, 40.69336192556367]}
+        onClick={this._onClickMap}
+      >
+
+        <DrawControl
+            position="top-left"
+            onDrawCreate={this.onDrawCreate}
+            onDrawUpdate={this.onDrawUpdate}
+          />
+
+
+        {/*<GeoJSONLayer {...geojsonStyles} data={geojson} />*/}
+          
+          <Slider
+            step={0.0001}
+            valueLabelDisplay="on"
+            aria-label="custom thumb label"
+            defaultValue={0.037}
+            max={1.5}
+            min={0.005}
+            style={{
+              width: '230px',
+              padding: '220px 15px 10px 0',
+              margin: '15px'
+            }}
+            value={value}
+            aria-labelledby="label"
+            onChange={this.handleChange}
+          />
+
+      </Map>
     </div>
+
   );
 }
-
-export default App;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,105 @@
+/*import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import ReactMapboxGl, { GeoJSONLayer } from "react-mapbox-gl";
+import DrawControl from 'react-mapbox-gl-draw';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+
+import "./styles.css";
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiYWhtZWRiaDkiLCJhIjoiY2s1bzZkbmQ1MDNzMTNzcWw0eXJpc3duNiJ9.RcZZI2_JbPoeFp-14N8y8w'
+
+});
+class App extends Component {
+  onDrawCreate = ({ features }) => {
+    console.log(features);
+  };
+
+  onDrawUpdate = ({ features }) => {
+    console.log({ features });
+  };
+
+render() {
+    const geojson = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {
+            text: "Fort Greene"
+          },
+          geometry: {
+            type: "Polygon",
+            coordinates: [
+              [
+                [-73.97777080535889, 40.69336192556367],
+                [-73.97704124450682, 40.68986390865585],
+                [-73.97315740585327, 40.68970120572578],
+                [-73.97388696670532, 40.69323177008439],
+                [-73.97777080535889, 40.69336192556367]
+              ]
+            ]
+          }
+        }
+      ]
+    };
+  const geojsonStyles = {
+      lineLayout: {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      linePaint: {
+        "line-color": "#ff11ff",
+        "line-width": 4,
+        "line-opacity": 1
+      },
+      symbolLayout: {
+        "text-field": "{text}",
+        "symbol-placement": "line",
+        "text-rotation-alignment": "map",
+        "text-size": {
+          base: 1,
+          stops: [[9, 9], [14, 12]]
+        }
+      },
+      symbolPaint: {
+        "text-color": "rgba(0, 0, 0, 1)",
+        "text-halo-color": "rgba(255, 255, 255, 1)",
+        "text-halo-width": 2
+      }
+    };
+  return (
+    <div className="App">
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        //center={[28.6864, 41.0391]}
+        containerStyle={{
+          height: '80vh',
+          width: '100vw'
+        }}
+        zoom={[16]}
+        center={[-73.9757752418518, 40.69144210646147]}
+      >
+        <DrawControl
+            position="top-left"
+            onDrawCreate={this.onDrawCreate}
+            onDrawUpdate={this.onDrawUpdate}
+          />
+          <GeoJSONLayer {...geojsonStyles} data={geojson} />
+      </Map>
+    </div>
+  );
+}
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);*/
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,3 +112,56 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+ 
+mapboxgl.accessToken = 
+'pk.eyJ1IjoiYWhtZWRiaDkiLCJhIjoiY2s1bzZkbmQ1MDNzMTNzcWw0eXJpc3duNiJ9.RcZZI2_JbPoeFp-14N8y8w';
+ 
+class Application extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lng: 28.6864 ,
+			lat:  41.0391,
+			zoom: 10.89
+		};
+	}
+ 
+	componentDidMount() {
+		const map = new mapboxgl.Map({
+			container: this.mapContainer,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [this.state.lng, this.state.lat],
+			zoom: this.state.zoom
+		});
+	 
+		map.on('move', () => {
+			this.setState({
+				lng: map.getCenter().lng.toFixed(4),
+				lat: map.getCenter().lat.toFixed(4),
+				zoom: map.getZoom().toFixed(2)
+				});
+		});
+	}
+	 
+	render() {
+		return (
+			<div>
+				<div className='sidebarStyle'>
+					<div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+				</div>
+				<div ref={el => this.mapContainer = el} className='mapContainer' />
+				</div>
+		)
+	}
+}
+ 
+ReactDOM.render(<Application />, document.getElementById('app'));*/
